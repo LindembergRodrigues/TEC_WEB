d2674117898690a1865e8de96aac1f929a59f6a4
Lindemberg Rodrigues <bergfla2010@hotmail.com>
2024-07-14 18:27:06 -0300

    init

---
+++ Projeto_TEC_WEB/Server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/',(req, res) => {
+	res.send('Hello world!');
+});
+
+server.listen(port,hostname, ()=>{
+	console.log('Server running at htt://localhost:${port}/');
+})
